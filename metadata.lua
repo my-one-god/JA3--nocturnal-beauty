1275788f702c6cea2c44d3d748b956060277d497
MyOneGod <MyOneGod@proton.me>
2024-06-24 20:22:18 +0200

    initial commit

---
+++ metadata.lua
@@ -0,0 +1,10 @@
+return PlaceObj('ModDef', {
+	'title', "[MOG] Nocturnal Beauty",
+	'id', "Tpi36ru",
+	'author', "MyOneGod",
+	'version', 1,
+	'lua_revision', 233360,
+	'saved_with_revision', 350233,
+	'default_options', {},
+	'saved', 1719247178,
+})
